fcde54220d1aad0a0bfa6ee243a85cd0ae85ad37
Jennifer Li <jhli3@illinois.edu>
2019-11-29 15:11:09 -0500

    Creating UIScreen Activity XML

---
+++ app/src/main/java/com/example/cs_125_project_final/UIScreen.java
@@ -0,0 +1,16 @@
+package com.example.cs_125_project_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UIScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uiscreen);
+    }
+
+
+}
