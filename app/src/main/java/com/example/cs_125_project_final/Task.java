12e364e60b8d1e911f43c72647c3d0f043bfd8b4
eeshamoona <emoona2@illinois.edu>
2019-11-28 19:11:52 -0500

    Created Task.class

---
+++ app/src/main/java/com/example/cs_125_project_final/Task.java
@@ -0,0 +1,23 @@
+package com.example.cs_125_project_final;
+
+/**
+ * Task Class used to process the data the user enters on the task screen.
+ */
+public class Task {
+    //create task screen layout
+        //includes a scrolling list of verbs
+        //a text box for the user to fill in
+        //another scrolling list of times
+        //add button
+    //onPress of the add button
+        //make sure that there has been a selection of verbs/ text/ time
+        //if yes,
+            //compile all the information from the components into a String
+            //addTask to JSON file using the taskAPI
+            //tell user that it has been added --> notification? Alert Dialog?
+            //clear all the information so the user can add another.
+        //if no,
+            //Alert Dialog? Somehow tell the user to fill out the info
+
+    //post the JSON file to the Task Web API for the UI screen to use
+}
