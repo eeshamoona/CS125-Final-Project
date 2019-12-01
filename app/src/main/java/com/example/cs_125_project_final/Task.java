7d16641558f1277aa1c3ca1b717439f262578023
eeshamoona <emoona2@illinois.edu>
2019-12-01 04:10:07 -0600

    Populated Spinners

--- app/src/main/java/com/example/cs_125_project_final/Task.java
+++ app/src/main/java/com/example/cs_125_project_final/Task.java
@@ -2,6 +2,7 @@ package com.example.cs_125_project_final;
 
 import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -26,8 +27,26 @@ public class Task extends AppCompatActivity {
         //add button
 
         Spinner verbs = findViewById(R.id.Verbs);
-        Spinner time = findViewById(R.id.Time);
+            // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.verbs_array, android.R.layout.simple_spinner_item);
+            // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            // Apply the adapter to the spinner
+        verbs.setAdapter(adapter);
+
         TextView text = findViewById(R.id.TaskEnter);
+
+        Spinner time = findViewById(R.id.Time);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this,
+                R.array.time_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        time.setAdapter(adapter2);
+
+
         Button addbutton = findViewById(R.id.Add);
         addbutton.setOnClickListener(unused -> {
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
@@ -35,12 +54,30 @@ public class Task extends AppCompatActivity {
             if (verbs.getSelectedItemPosition() != -1
                     && !(text.getText().equals(""))
                     && time.getSelectedItemPosition() != -1) {
-            }
-            else {
+                String creatingTask = "";
+                creatingTask = verbs.getSelectedItem().toString() +
+                        text.getText().toString() + time.getSelectedItem().toString();
+                int returnInt = postInfo(creatingTask);
+                if (returnInt == -1) {
+                    dialog.setMessage("Error in Building Your Task");
+                    dialog.show();
+                } else {
+                    dialog.setMessage("Task Created Successfully");
+                    dialog.show();
+                }
+                verbs.setSelection(-1);
+                text.setText("");
+                time.setSelection(-1);
+            } else {
                 dialog.setMessage("Please Fill out all information");
                 dialog.show();
             }
         });
+    }
+
+    public int postInfo (String toPost) {
+
+        return 1;
     }
 
     //onPress of the add button
