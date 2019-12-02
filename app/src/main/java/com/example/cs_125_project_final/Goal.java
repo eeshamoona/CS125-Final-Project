9a2614214487c85a96ed463e179b1c08bcaf97e5
eeshamoona <emoona2@illinois.edu>
2019-12-02 15:36:59 -0600

    Added Home and Goal Screen

--- app/src/main/java/com/example/cs_125_project_final/Goal.java
+++ app/src/main/java/com/example/cs_125_project_final/Goal.java
@@ -3,12 +3,14 @@ package com.example.cs_125_project_final;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Home extends AppCompatActivity {
+/**
+ * Screen from for Creating a new Goal.
+ */
+public class Goal extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-
+        setContentView(R.layout.activity_goal);
     }
 }
