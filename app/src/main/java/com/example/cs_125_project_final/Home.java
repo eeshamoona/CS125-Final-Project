09a8a7a715f2fb6d21ca5819d5dab46918f11287
eeshamoona <emoona2@illinois.edu>
2019-12-02 21:29:15 -0600

    More additions to the Goal and Home Screens

--- app/src/main/java/com/example/cs_125_project_final/Home.java
+++ app/src/main/java/com/example/cs_125_project_final/Home.java
@@ -1,6 +1,10 @@
 package com.example.cs_125_project_final;
 
 import android.os.Bundle;
+
+import android.content.Intent;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Home extends AppCompatActivity {
@@ -8,6 +12,10 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Button addGoal = findViewById(R.id.addGoal);
+        addGoal.setOnClickListener(unused -> startActivity(new Intent(this, Goal.class)));
+
 
 
     }
